a834fe7226277d4b883728bbf070c195a342eae2
Hayato-7812 <haya12to17@gmail.com>
2022-09-12 23:29:25 +0900

    同じユーザが3回より多く連続でコメントできないようにした

--- chatapp/public/javascripts/publish.js
+++ chatapp/public/javascripts/publish.js
@@ -23,19 +23,23 @@ function publish() {
 // サーバから受信した投稿メッセージを画面上に表示する
 
 socket.on('recieveMessage', function (data) {
-    const now =  new Date();
-    const year = now.getFullYear();
-    const month = now.getMonth()+1;
-    const date = now.getDate();
-    const hour = now.getHours();
-    const min = now.getMinutes();
+    if (data === "The maximum number of consecutive transmissions") {
+        alert("3回より多く連続で同じユーザが投稿できません")
+    } else {
+        const now = new Date();
+        const year = now.getFullYear();
+        const month = now.getMonth() + 1;
+        const date = now.getDate();
+        const hour = now.getHours();
+        const min = now.getMinutes();
     
-    // 現在入出しているユーザー名を所得
-    const userName = $('#userName').val();
-    if(data[0] === userName){
-        $('#thread').prepend('<p class="threadd right">' + data[0]+ 'さん:' +'<br>' + data[1] + '<br>' + '('+year+'/'+month+'/'+date+'/'+hour+':'+min+')' + '</p>');
-    }else{
-        $('#thread').prepend('<p class="threadd left">' + data[0]+ 'さん:' + '<br>' +data[1] + '<br>' + '('+year+'/'+month+'/'+date+'/'+hour+':'+min+')' + '</p>');
+        // 現在入出しているユーザー名を所得
+        const userName = $('#userName').val();
+        if (data[0] === userName) {
+            $('#thread').prepend('<p class="threadd right">' + data[0] + 'さん:' + '<br>' + data[1] + '<br>' + '(' + year + '/' + month + '/' + date + '/' + hour + ':' + min + ')' + '</p>');
+        } else {
+            $('#thread').prepend('<p class="threadd left">' + data[0] + 'さん:' + '<br>' + data[1] + '<br>' + '(' + year + '/' + month + '/' + date + '/' + hour + ':' + min + ')' + '</p>');
+        }
     }
 
 });
