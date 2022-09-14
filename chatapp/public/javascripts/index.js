63f855530a59debf9b7e49f17bb9df28526ab78c
haruto <hori8610@i.softbank.jp>
2022-09-14 12:12:01 +0900

    resolve conflict

--- chatapp/public/javascripts/index.js
+++ chatapp/public/javascripts/index.js
@@ -15,4 +15,14 @@ function enter() {
     //userName == "" ? alert("ユーザ名を入力してください。") : $('form').submit();
 }
 
+socket.on('OK', function () {
+    $('form').submit();
+});
+
+// ユーザ名に重複があった場合に帰ってくる信号 --> エラー表示
+socket.on('NO', function (data) {
+    $('.errorMessage').remove();
+    $('#sameNameError').append('<p class="errorMessage">既にユーザ名："' + data + '"が存在するので、別の名前でログインして下さい</p>');
+});
+
 
