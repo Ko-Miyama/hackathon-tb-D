5e00e85a325abf9b922fd9667a829b634c94ac3b
tsudakogo <tsuda-kougo@ed.tmu.ac.jp>
2022-09-15 03:18:39 +0900

    [add]ShortcutKeyEvent

--- chatapp/public/javascripts/PublishEnterKey.js
+++ chatapp/public/javascripts/PublishEnterKey.js
@@ -4,9 +4,14 @@
 window.addEventListener("keyup",KeyUp)
 
 function KeyUp(event){
-    if(event.shiftKey){
-		if(event.key=='Enter'){
-            publish();
-		}
+  if(event.shiftKey){
+    if(event.key=='Enter'){
+        publish();
     }
+  }
+  if(event.ctrlKey){
+    if(event.key=='Enter'){
+        memo();
+    }
+  }
 }
