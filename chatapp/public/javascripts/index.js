8b8e8609c1b762cbe1edea6ba86d830b1643afc3
haruto <hori8610@i.softbank.jp>
2022-09-14 11:51:31 +0900

    [add] layout_login

--- chatapp/public/javascripts/index.js
+++ chatapp/public/javascripts/index.js
@@ -4,7 +4,7 @@
 function enter() {
     // 入力されたユーザ名を取得する
     const userName = $('#userName').val();
-    
+ 
     // ユーザ名が未入力でないかチェックする
     if(userName.length === 0 || !userName.match(/\S/g)){
         alert("ユーザ名を入力してください。");
@@ -14,3 +14,5 @@ function enter() {
     
     //userName == "" ? alert("ユーザ名を入力してください。") : $('form').submit();
 }
+
+
